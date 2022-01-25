688f19d44844a6c6921655c07c97b36e68699a53
michalosman <michal.osman.997@gmail.com>
2022-01-25 13:16:47 +0100

    Finish Navbar design

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import './Navbar.css'
-
+import logo from '../../assets/images/logo.png'
 const Navbar = () => {
   return (
     <header className="navbar">
-      <h1>Social Network</h1>
-      <div className="user-panel">
-        <p>Username</p>
+      <div className="nav-panel">
+        <img className="logo" src={logo} alt="logo" />
+        <h1 className="page-name">social network</h1>
+      </div>
+      <div className="nav-panel">
+        <p>Michał Osman</p>
         <button className="button">Log out</button>
       </div>
     </header>
