11f645513ea98275c7b9c30251c0e06093efce92
michalosman <michal.osman.997@gmail.com>
2022-01-26 15:16:46 +0100

    Create users API & auth

---
+++ server/routes/users.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import {
+  signUp,
+  signIn,
+  warnUser,
+  blockUser,
+  getUsers,
+  validateRole,
+} from '../controllers/userController'
+import { auth, authModerator, authAdmin } from '../middleware/auth'
+
+const router = express.Router()
+
+router.get('/', auth, getUsers)
+
+router.post('/signUp', signUp)
+router.post('/signIn', signIn)
+router.post('/validateRole', auth, validateRole)
+
+router.put('/warn/:userId', auth, authModerator, warnUser)
+router.put('/block/:userId', auth, authAdmin, blockUser)
+
+export default router
