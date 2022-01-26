3693f8e1799cf5073331897cb9379216552a853a
michalosman <michal.osman.997@gmail.com>
2022-01-26 15:13:58 +0100

    Add error handler

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import errorHandler from './error/errorHandler'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -11,6 +12,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
